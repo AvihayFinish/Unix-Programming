59a9a6e3c2a6a4ad30dac4e92c9c91d03dd8d736
AvihayFinish <119002109+AvihayFinish@users.noreply.github.com>
2024-08-11 14:54:58 +0300

    Update find.c

--- partC/find.c
+++ partC/find.c
@@ -19,38 +19,47 @@ typedef struct comparison_result {
 } cmp;
 
 cmp* the_number(const char*);
-void pass_over_files(const char*, int, int, int, cmp*);
+void pass_over_files(const char*, int, int, int, cmp*, char*);
 int save_sub_dirs (char**, const char*, const char*, int);
 void f_empty(struct stat*, const char*);
 void f_inum(struct stat*, cmp*, const char*);
 void f_links(struct stat*, cmp*, const char*);
+void f_type(mode_t md, char*, const char*);
 
 /* This program make the find command. for this time I implement only the
-* inum, links and empty test without options. from time to time I will
+* inum, links, empty and type test without options. from time to time I will
 * add more tests and options to this program.
 */
 int main (int argc, char * argv[]) {
-    char* start_point = argv[1];
+    char* start_point = ".";
     int empty = 0;
     int inum = 0;
     int links = 0;
     int n = -1;
+    char * type = NULL;
     cmp* rs = NULL;
-    for (int i = 2; i < argc; i++) {
+    for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-empty") == 0) {
             empty = 1;
             break;
         }
         else if (strcmp(argv[i], "-inum") == 0) {
             inum = 1;
+            rs = the_number(argv[++i]);
         }
         else if (strcmp(argv[i], "-links") == 0) {
             links = 1;
+            rs = the_number(argv[++i]);
         }
-        rs = the_number(argv[++i]);
+        else if (strcmp(argv[i], "-type") == 0) {
+            type = argv[++i];
+        }
+        else {
+            start_point = argv[i];
+        }
     }
 
-    pass_over_files(start_point, empty, inum, links, rs);
+    pass_over_files(start_point, empty, inum, links, rs, type);
 
     return 0;
 }
@@ -80,7 +89,7 @@ cmp* the_number(const char* str) {
 /* This function pass over the enrtys of the given directory and call the
 *  test for the files by the ints that the function get.
 */
-void pass_over_files(const char* dirname, int empty, int inum, int links, cmp* rs) {
+void pass_over_files(const char* dirname, int empty, int inum, int links, cmp* rs, char* type) {
     DIR *dirptr;
     struct dirent *dirnerpt;
     int count_subdirs = 0;
@@ -127,7 +136,10 @@ void pass_over_files(const char* dirname, int empty, int inum, int links, cmp* r
         }
         else if (links) {
             f_links(&st, rs, dirnerpt->d_name);
-        }       
+        } 
+        else if (type != NULL) {
+            f_type(st.st_mode, type, dirnerpt->d_name);
+        }      
     }
 
     // if the current directory have a sub directories, call recursively
@@ -135,7 +147,7 @@ void pass_over_files(const char* dirname, int empty, int inum, int links, cmp* r
     if (count_subdirs > 0) {
         for (int i = 0; i < count_subdirs; i++) {
             printf("\n*** %s ***\n", subdirs[i]);
-            pass_over_files(subdirs[i], empty, inum, links, rs);
+            pass_over_files(subdirs[i], empty, inum, links, rs, type);
             free(subdirs[i]);
         }
     }
@@ -209,5 +221,44 @@ void f_links(struct stat* st, cmp* rs, const char* filename) {
             printf("%s %-8ld\n", filename, st->st_nlink);
         }
         break;
+    }
+}
+
+/* This function print the file name if the file is from the type that
+* given by the user.
+*/
+void f_type(mode_t md, char* type, const char* filename) {
+    if (strcmp(type, "d") == 0) {
+        if (S_ISDIR(md)) 
+            printf("%s\n", filename);
+    }
+    else if (strcmp(type, "c") == 0) {
+        if (S_ISCHR(md))
+            printf("%s\n", filename);
+    }
+    else if (strcmp(type, "f") == 0) {
+        if (S_ISREG(md))
+           printf("%s\n", filename);
+    }
+    else if (strcmp(type, "l") == 0) {
+        if (S_ISLNK(md))
+            printf("%s\n", filename);
+    }
+    else if (strcmp(type, "b") == 0) {
+        if (S_ISBLK(md))
+            printf("%s\n", filename);
+    }
+    else if (strcmp(type, "p") == 0) {
+        if (S_ISFIFO(md))
+            printf("%s\n", filename);
+    }
+    else if (strcmp(type, "s") == 0) {
+        if (S_ISSOCK(md))
+            printf("%s\n", filename);
+    } 
+    else {
+        printf("the option for type is: d-directory, f-regular file,\n"
+                "c-character, l-symbolic link, b-block, p-pipe, s-socket");
+        exit(EXIT_FAILURE);
     }
 }
